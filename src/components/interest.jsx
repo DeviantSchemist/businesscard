4526cd67ae9a210cb2e9fb59ad7cf69dcbf0ea33
Michael Ngo <michaelngo1337@yahoo.com>
2023-03-13 14:26:27 -0700

    make interest component

---
+++ src/components/interest.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Interest() {
+  return (
+    <section className="bg-[#1A1B21]">
+      <h2 className="text-base text-[#F5F5F5] font-bold">Interests</h2>
+      <p className="text-[#DCDCDC] text-[10.24px] w-[247px]">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+    </section>
+  )
+}
