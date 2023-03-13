8e9775cf0ffe43d08dda2488491479d667a425d3
Michael Ngo <michaelngo1337@yahoo.com>
2023-03-13 14:31:28 -0700

    changed footer background color

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ import Github from "../assets/github.png";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 flex justify-center gap-x-8 py-5">
+    <footer className="bg-[#161619] flex justify-center gap-x-8 py-5">
       <img src={Twitter} alt="Twitter" />
       <img src={Facebook} alt="Facebook" />
       <img src={Instagram} alt="Instagram" />
