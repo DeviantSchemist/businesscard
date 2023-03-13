dde5914c73eeaeff267104941966636b83c9d43b
Michael Ngo <michaelngo1337@yahoo.com>
2023-03-13 00:40:47 -0700

    made footer component

---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+import Twitter from "../assets/twitter.png";
+import Facebook from "../assets/facebook.png";
+import Instagram from "../assets/instagram.png";
+import Github from "../assets/github.png";
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 flex justify-center gap-x-8 py-5">
+      <img src={Twitter} alt="Twitter" />
+      <img src={Facebook} alt="Facebook" />
+      <img src={Instagram} alt="Instagram" />
+      <img src={Github} alt="Github" />
+    </footer>
+  )
+}
